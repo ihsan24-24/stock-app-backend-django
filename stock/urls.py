e95b581ad36607705cea2ccfc9d3c97f226fada5
ihsan <hasansaygin24@gmail.com>
2023-03-27 12:47:35 +0300

    stock urls.py created

---
+++ stock/urls.py
@@ -0,0 +1,15 @@
+from rest_framework import routers
+from .views import CategoryView, BrandView, FirmView, ProductView, PurchaseView, SalesView
+
+router = routers.DefaultRouter()
+
+router.register("categories", CategoryView)
+router.register("brands", BrandView)
+router.register("firms", FirmView)
+router.register("products", ProductView)
+router.register("purchases", PurchaseView)
+router.register("sales", SalesView)
+
+urlpatterns = [
+    
+] + router.urls
